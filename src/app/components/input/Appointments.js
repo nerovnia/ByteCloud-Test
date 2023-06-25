0ba9628c0fae5113ef7577ea34b9946fd34a3989
Volodymyr Nerovnia <nerv@i.ua>
2023-06-25 13:13:55 +0300

    Added onPaste to: - Appointments.js - Doctors.js - Patients.js

--- src/app/components/input/Appointments.js
+++ src/app/components/input/Appointments.js
@@ -1,6 +1,6 @@
 export default function Appointments(props) {
   return (<>
       <p>Appointments</p>
-      <textarea rows="15" cols="30" onChange={props.onAppointments}></textarea>
+      <textarea rows="15" cols="30" onChange={props.onAppointments} onPaste={props.onAppointments}></textarea>
   </>)
 }
--- src/app/components/input/Patients.js
+++ src/app/components/input/Patients.js
@@ -1,6 +1,6 @@
 export default function Patients(props) {
   return (<>
     <p>Patients</p>
-    <textarea rows="15" cols="30" onChange={props.onPatients}></textarea>
+    <textarea rows="15" cols="30" onChange={props.onPatients} onPaste={props.onPatients}></textarea>
   </>)
 }
