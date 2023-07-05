84c7bebee769ccb16dbf4391adfae9ccb50b9482
Volodymyr Nerovnia <nerv@i.ua>
2023-07-05 21:27:51 +0300

    Configured app for mongoose

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    esmExternals: "loose", 
+    serverComponentsExternalPackages: ["mongoose"]
+  },
+  
+  /*
+  webpack: (config) => {
+    config.experiments = {
+      topLevelAwait: true
+    };
+    return config;
+  },
+  */
+}
 
 module.exports = nextConfig
