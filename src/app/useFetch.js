90c224126b21262ae64f44343f73cd898627f75f
Volodymyr Nerovnia <nerv@i.ua>
2023-07-13 17:45:35 +0300

    Improved useFetch module

--- src/app/useFetch.js
+++ src/app/useFetch.js
@@ -1,19 +1,52 @@
+import { useState } from "react";
+
 export default function useFetch(baseUrl) {
+  //const [loading, setLoading] = useState(true);
+
   function get(url) {
-      return fetch(baseUrl + url)
-              .then(response => response.json());
+    return new Promise((resolve, reject) => {
+      fetch(baseUrl + url)
+        .then(response => response.json())
+        .then(data => {
+          if (!data) {
+            //setLoading(false);
+            return reject(data);
+          }
+          //setLoading(false);
+          resolve(data);
+        })
+        .catch(error => {
+          //setLoading(false);
+          reject(error);
+        });
+    });
   }
 
   function post(url, body) {
-      return fetch(baseUrl + url, {
-          method: "post",
-          headers: {
-              "Content-Type": "application/json"
-          },
-          body: JSON.stringify(body)
+    return new Promise((resolve, reject) => {
+      fetch(baseUrl + url, {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body)
       })
-      .then(response => response.json());
+        .then(response => response.json())
+        .then(data => {
+          if (!data) {
+            //setLoading(false);
+            return reject(data);
+          }
+          //setLoading(false);
+          resolve(data);
+        })
+        .catch(error => {
+          //setLoading(false);
+          reject(error);
+        });
+    });
   }
 
   return { get, post };
+//  return { get, post, loading };
 };
