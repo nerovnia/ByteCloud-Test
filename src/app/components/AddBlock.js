e0f7aacddb64b84c223540a2e334aa012cf7faa6
Volodymyr Nerovnia <nerv@i.ua>
2023-07-14 06:23:03 +0300

    Added autoFill function and button to AddBlock

--- src/app/components/AddBlock.js
+++ src/app/components/AddBlock.js
@@ -1,11 +1,12 @@
 import Appointments from "./input/Appointments";
 import Doctors from "./input/Doctors";
 import Patients from "./input/Patients";
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import { validateRegisterRecords } from "../service/validators";
 import SaveModal from "./report/SaveModal";
+import Fetch from "@/app/useFetch"
 
-import { InferGetServerSidePropsType, GetServerSideProps } from 'next';
+import { InferGetServerSidePropsType, GetServerSideProps } from "next";
 //import { addCollection } from "@/utils/dbOperations";
 
 export default function AddBlock() {
@@ -14,34 +15,41 @@ export default function AddBlock() {
   const [saveModalShow, setSaveModalShow] = useState(false);
 
   // ----------------------------- Delete in production
-  useEffect(() => {
-  }, [registrationData]);
+  useEffect(() => {}, [registrationData]);
   // -----------------------------
 
+  function handleAutofill() {
+    const { get } = Fetch("/api/");
+    get("test-data").then((testData) => {
+      //console.log(testData.body.data);
+      setRegistrationDataForModal(testData.body.data);
+    });
+  }
+
   function handleChangePatients(event) {
-    const {doctors, appointments} = registrationData;
+    const { doctors, appointments } = registrationData;
     setRegistrationData({
       doctors,
       appointments,
-      patients: event.target.value
+      patients: event.target.value,
     });
   }
 
   function handleChangeDoctors(event) {
-    const {patients, appointments} = registrationData;
+    const { patients, appointments } = registrationData;
     setRegistrationData({
       doctors: event.target.value,
       appointments,
-      patients
+      patients,
     });
   }
 
   function handleChangeAppointments(event) {
-    const {patients, doctors} = registrationData;
+    const { patients, doctors } = registrationData;
     setRegistrationData({
       doctors,
       appointments: event.target.value,
-      patients
+      patients,
     });
   }
 
@@ -51,7 +59,7 @@ export default function AddBlock() {
     setRegistrationDataForModal(registrationData);
     setSaveModalShow(true);
     document.querySelector("#frmAddRegistrationData").reset();
-    setRegistrationData({});
+    //setRegistrationData({});
   }
 
   function handleClearDB() {
@@ -69,26 +77,68 @@ export default function AddBlock() {
         <div className="container text-center">
           <div className="row">
             <div className="col">
-              <Patients dataTest="patients" onPatients={handleChangePatients}></Patients>
+              <Patients
+                dataTest="patients"
+                onPatients={handleChangePatients}
+              ></Patients>
             </div>
             <div className="col">
-              <Doctors dataTest="doctors" onDoctors={handleChangeDoctors}></Doctors>
+              <Doctors
+                dataTest="doctors"
+                onDoctors={handleChangeDoctors}
+              ></Doctors>
             </div>
             <div className="col">
-              <Appointments dataTest="appointments" onAppointments={handleChangeAppointments}></Appointments>
+              <Appointments appointments={{...registrationDataForModal.appointments}}
+                dataTest="appointments"
+                onAppointments={handleChangeAppointments}
+              ></Appointments>
             </div>
           </div>
         </div>
         <div className="d-grid gap-2 d-md-flex justify-content-md-end mt-2 mb-2">
-          <button id="sendData" className="btn btn-primary me-md-2" type="button" onClick={handleSendData}>
+          <button
+            id="autoFill"
+            className="btn btn-primary me-md-2"
+            type="button"
+            onClick={handleAutofill}
+          >
+            Autofill
+          </button>
+
+          <button
+            id="sendData"
+            className="btn btn-primary me-md-2"
+            type="button"
+            onClick={handleSendData}
+          >
             Send Data
           </button>
-          <button id="clearDB" className="btn btn-primary" type="button" onClick={handleClearDB}>
+          <button
+            id="clearDB"
+            className="btn btn-primary"
+            type="button"
+            onClick={handleClearDB}
+          >
             Clear DB
           </button>
         </div>
       </form>
-      <SaveModal show={saveModalShow} reportdata={validateRegisterRecords(registrationDataForModal)} onHide={() => saveModalClose()} />
+      {
+        /*
+      <SaveModal
+      show={saveModalShow}
+      reportdata={validateRegisterRecords(registrationDataForModal)}
+      onHide={() => saveModalClose()}
+    />
+      */
+
+      }
+      <SaveModal
+        show={saveModalShow}
+        reportdata={validateRegisterRecords(registrationData)}
+        onHide={() => saveModalClose()}
+      />
     </>
   );
 }
